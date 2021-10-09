1b2b792684f3486db158c14c669f4357151e7b40
kishori <imkish.desh@gmail.com>
2021-10-09 10:50:44 +0530

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,35 @@
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+// const express = require('express');
+// const app = express();
+// // const app = express();
+// app.use(requireHTTPS);
+
+// app.use(express.static('/dist/LetsShop'));
+
+// app.get('/*', function (req, res) {
+//     res.sendFile('/src/index.html', { root: '/dist/LetsShop' }
+//     );
+// });
+// app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/LetsShop'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/LetsShop/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
